fcf97966855533aa419afe3c6ac955f59db45515
IlyaIvanov7567 <ilyaivanov7567@gmail.com>
2021-09-29 10:20:59 +0300

    3.1. Реализована функция вычисления числа Фибоначчи с использованием рекурсии

---
+++ Lesson1_Task3.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+// Вычисление числа Фибоначчи с использованием рекурсии
+
+int n;
+Console.Write("Введите число: ");
+n = int.Parse(Console.ReadLine());
+
+Console.WriteLine("{0} член последовательности Фибоначчи равен {1}", n, Fibonacci(n));
+Console.ReadKey();
+
+static int Fibonacci(int n)
+{
+    if (n == 0)
+        return 0;
+    else if (n == 1)
+        return 1;
+    else
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
