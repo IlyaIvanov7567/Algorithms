5e51f5b783f227b677f1d31eaf9858a18fd1850c
IlyaIvanov7567 <ilyaivanov7567@gmail.com>
2021-09-29 10:22:29 +0300

    3.2. Реализована функция вычисления числа Фибоначчи с использованием цикла

---
+++ Lesson1_Task3.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+// Вычисление числа Фибоначчи с использованием цикла
+
+int n;
+int prev = 0;
+int current = 1;
+int temp;
+int result = 0;
+
+Console.Write("Введите число: ");
+n = int.Parse(Console.ReadLine());
+
+
+for (int i = 2; i <= n; i++)
+{
+    result = prev + current;
+    temp = current;
+    current = current + prev;
+    prev = temp;
+}
+
+Console.WriteLine("{0} член последовательности Фибоначчи равен {1}", n, result);
+Console.ReadKey();
