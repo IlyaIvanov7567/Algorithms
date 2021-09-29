abb8e3db8e3b1b84c45761edb797edabcbc2e1e6
IlyaIvanov7567 <ilyaivanov7567@gmail.com>
2021-09-29 10:19:25 +0300

    2. Вычислена асимптотическая сложность функции

---
+++ Lesson1_Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+// Асимптотическую сложность O(f(N) × g(N) × h(3N)) = O(f(N) × g(N) × h(N))
+
+public static int StrangeSum(int[] inputArray)
+{
+    int sum = 0;
+    for (int i = 0; i < inputArray.Length; i++)             // O(f(N))
+    {
+        for (int j = 0; j < inputArray.Length; j++)         // O(g(N))
+        {
+            for (int k = 0; k < inputArray.Length; k++)     // O(h(3N))
+            {
+                int y = 0;
+
+                if (j != 0)
+                {
+                    y = k / j;
+                }
+
+                sum += inputArray[i] + i + k + j + y;
+            }
+        }
+    }
+
+    return sum;
+}
