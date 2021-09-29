490b9d0e627baa2c086231a175457a9cb8056e22
IlyaIvanov7567 <ilyaivanov7567@gmail.com>
2021-09-29 10:16:36 +0300

    1. Реализация функции согласно блок-схеме

--- Lesson1_Task1/Program.cs
+++ Lesson1_Task1/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 
-namespace Lesson1_Task1
+// Функция согласно блок-схеме
+
+int num;
+Console.Write("Введите число: ");
+num = int.Parse(Console.ReadLine());
+
+int d = 0;
+int i = 2;
+
+while (i < num)
 {
-    class Program
+    if (num % i == 0)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        d++;
+        i++;
     }
+    else
+    {
+        i++;
+    }
+}
+if (d == 0)
+{
+    Console.WriteLine($"Число {num} простое");
+}
+else
+{
+    Console.WriteLine($"Число {num} не простое");
 }
