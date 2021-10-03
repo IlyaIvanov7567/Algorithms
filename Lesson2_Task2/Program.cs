e34577c90c319ffaa4508e5a73e4f554982b9636
IlyaIvanov7567 <ilyaivanov7567@gmail.com>
2021-10-03 23:39:41 +0300

    Lesson2_Task2. Биинарный поиск

---
+++ Lesson2_Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+// Асимптотическую сложность O(F(log inputArray.Length))
+
+var arr = new int[] { 1, 2, 4, 6, 8, 10, 12, 14, 15 };
+
+int searchValue = 4;
+Console.WriteLine("Индекс искомого элемента массива со значением {1} = {0}", BinarySearch(arr, searchValue), searchValue);
+
+int BinarySearch(int[] inputArray, int searchValue)
+{
+    int min = 0;
+    int max = inputArray.Length - 1;
+    while (min <= max)
+    {
+        int mid = (min + max) / 2;
+        if (searchValue == inputArray[mid])
+        {
+            return mid;
+        }
+        else if (searchValue < inputArray[mid])
+        {
+            max = mid - 1;
+        }
+        else
+        {
+            min = mid + 1;
+        }
+    }
+    return -1;
+}
+
