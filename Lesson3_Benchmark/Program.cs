3f9b9248a3edce40c624451acb7d1ec455b97a64
IlyaIvanov7567 <ilyaivanov7567@gmail.com>
2021-10-09 11:36:20 +0300

    Lesson3. Resolved.

--- Lesson3_Benchmark/Program.cs
+++ Lesson3_Benchmark/Program.cs
@@ -2,69 +2,140 @@
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
 
-/*
- * Что я делаю:
- * 1. Объявляю класс Точка public class Point
- * 2. Объявляю класс Бенчмарк, в котором буду замерять производительность public class BechmarkClass
- * 3. В классе Бенчмарк объявляю метод для рассчета расстояния между точками, 
- * который на вход принимает две точки public void Distance
- * 4. В классе Бенчмарк объявляю метод, производительность которого хочу померить public void TestDistance.
- * Метод на вход должен принимать массив точек и в цикле вызывать метод Distance,
- * передавая в него две точки (с четным индексом и не четным)
- * 
- * Вопросы:
- * 1. Как в бенчмарк передать массив точек?
- * Инициализировать массив циклом в классе BechmarkClass не получается - ошибка.
- * Инициализировать массив в методе Main получается, но не получаетсяя передать его в бенчмарк.
- * Инициализировать массив внутри тестируемого метода нельзя по условиям задачи.
- * Инициализацию массива произвожу циклом с использованием класса Random.
- * 
- * 2. Ругается на неправильную сигнатуру тестового метода - метод TestDistance не должен иметь аргументов.
- * А как тогда передать массив?
- */
-
 namespace Lesson3_test2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Point[] arrPoints = new Point[10];
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+        }
+    }
+
+    [RankColumn]
+    [MemoryDiagnoser]
+    public class BechmarkClass
+    {
+        //Обычный метод расчёта дистанции со ссылочным типом (PointClass — координаты типа float)
+        public static float DistanceClassFloat(PointClassFloat pointOne, PointClassFloat pointTwo)
+        {
+            float x = pointOne.X - pointTwo.X;
+            float y = pointOne.Y - pointTwo.Y;
+            return MathF.Sqrt((x * x) + (y * y)); 
+        }
+
+        //Обычный метод расчёта дистанции со значимым типом (PointStruct — координаты типа float)
+        public static float DistanceStructFloat(PointStructFloat pointOne, PointStructFloat pointTwo)
+        {
+            float x = pointOne.X - pointTwo.X;
+            float y = pointOne.Y - pointTwo.Y;
+            return MathF.Sqrt((x * x) + (y * y));
+        }
+
+        //Обычный метод расчёта дистанции со значимым типом (PointStruct — координаты типа double)
+        public static double DistanceStructDouble(PointStructDouble pointOne, PointStructDouble pointTwo)
+        {
+            double x = pointOne.X - pointTwo.X;
+            double y = pointOne.Y - pointTwo.Y;
+            return Math.Sqrt((x * x) + (y * y));
+        }
+
+        //Метод расчёта дистанции без квадратного корня со значимым типом(PointStruct — координаты типа float)
+        public static float DistanceStructFloat2(PointStructFloat pointOne, PointStructFloat pointTwo)
+        {
+            float x = pointOne.X - pointTwo.X;
+            float y = pointOne.Y - pointTwo.Y;
+            return (x * x) + (y * y);
+        }
+
+        [Benchmark]
+        public void TestDistanceClassFloat()
+        {
+            PointClassFloat[] arrPoints = new PointClassFloat[100];
 
             Random rnd = new();
 
             for (int i = 0; i < arrPoints.Length; i++)
             {
-                arrPoints[i] = new Point();
+                arrPoints[i] = new PointClassFloat();
                 arrPoints[i].X = (float)(rnd.NextDouble() * 10);
                 arrPoints[i].Y = (float)(rnd.NextDouble() * 10);
             }
-            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
-        }
-    }
 
-    public class BechmarkClass
-    {
-        public void Distance(Point pointOne, Point pointTwo)
-        {
-            float x = pointOne.X - pointTwo.X;
-            float y = pointOne.Y - pointTwo.Y;
-            var result = (float)Math.Sqrt((x * x) + (y * y));
-        }
-
-        [Benchmark]
-        public void TestDistance(Point[] arrPoints)
-        {
             for (int i = 0; i < arrPoints.Length / 2; i++)
             {
-                Distance(arrPoints[i * 2], arrPoints[i * 2 + 1]);
+                DistanceClassFloat(arrPoints[i * 2], arrPoints[i * 2 + 1]);
+            }
+        }
+        [Benchmark]
+        public void TestDistanceStructFloat()
+        {
+            PointStructFloat[] arrPoints = new PointStructFloat[100];
+
+            Random rnd = new();
+
+            for (int i = 0; i < arrPoints.Length; i++)
+            {
+                arrPoints[i].X = (float)(rnd.NextDouble() * 10);
+                arrPoints[i].Y = (float)(rnd.NextDouble() * 10);
+            }
+
+            for (int i = 0; i < arrPoints.Length / 2; i++)
+            {
+                DistanceStructFloat(arrPoints[i * 2], arrPoints[i * 2 + 1]);
+            }
+        }
+        [Benchmark]
+        public void TestDistanceStructDouble()
+        {
+            PointStructDouble[] arrPoints = new PointStructDouble[100];
+
+            Random rnd = new();
+
+            for (int i = 0; i < arrPoints.Length; i++)
+            {
+                arrPoints[i].X = rnd.NextDouble() * 10;
+                arrPoints[i].Y = rnd.NextDouble() * 10;
+            }
+
+            for (int i = 0; i < arrPoints.Length / 2; i++)
+            {
+                DistanceStructDouble(arrPoints[i * 2], arrPoints[i * 2 + 1]);
+            }
+        }
+        [Benchmark]
+        public void TestDistanceStructFloat2()
+        {
+            PointStructFloat[] arrPoints = new PointStructFloat[100];
+
+            Random rnd = new();
+
+            for (int i = 0; i < arrPoints.Length; i++)
+            {
+                arrPoints[i].X = (float)(rnd.NextDouble() * 10);
+                arrPoints[i].Y = (float)(rnd.NextDouble() * 10);
+            }
+
+            for (int i = 0; i < arrPoints.Length / 2; i++)
+            {
+                DistanceStructFloat2(arrPoints[i * 2], arrPoints[i * 2 + 1]);
             }
         }
     }
 
-    public class Point
+    public class PointClassFloat
     {
         public float X { get; set; }
         public float Y { get; set; }
+    }
+    public struct PointStructFloat
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+    }
+    public struct PointStructDouble
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
     }
 }
---
+++ Lesson3_StopWatch/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Lesson3_StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new();
+
+            PointClassFloat[] pointsClassFloat = new PointClassFloat[100];
+            PointStructFloat[] pointsStructFloat = new PointStructFloat[100];
+            PointStructDouble[] pointsStructDouble = new PointStructDouble[100];
+
+            for (int i = 0; i < pointsClassFloat.Length; i++)
+            {
+                pointsClassFloat[i] = new PointClassFloat();
+                pointsClassFloat[i].X = (float)(rnd.NextDouble() * 10);
+                pointsClassFloat[i].Y = (float)(rnd.NextDouble() * 10);
+            }
+
+            for (int i = 0; i < pointsStructFloat.Length; i++)
+            {
+                pointsStructFloat[i].X = (float)(rnd.NextDouble() * 10);
+                pointsStructFloat[i].Y = (float)(rnd.NextDouble() * 10);
+            }
+
+            for (int i = 0; i < pointsStructDouble.Length; i++)
+            {
+                pointsStructDouble[i].X = rnd.NextDouble() * 10;
+                pointsStructDouble[i].Y = rnd.NextDouble() * 10;
+            }
+
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            for (int i = 0; i < pointsClassFloat.Length / 2; i++)
+            {
+                DistanceClassFloat(pointsClassFloat[i * 2], pointsClassFloat[i * 2 + 1]);
+            }
+            stopWatch.Stop();
+            Console.WriteLine("Обычный метод расчёта дистанции со ссылочным типом (PointClass — координаты типа float). Затраченное время: {0}", stopWatch.Elapsed);
+
+            stopWatch.Restart();
+            for (int i = 0; i < pointsStructFloat.Length / 2; i++)
+            {
+                DistanceStructFloat(pointsStructFloat[i * 2], pointsStructFloat[i * 2 + 1]);
+            }
+            stopWatch.Stop();
+            Console.WriteLine("Обычный метод расчёта дистанции со значимым типом (PointStruct — координаты типа float). Затраченное время: {0}", stopWatch.Elapsed);
+
+            stopWatch.Restart();
+            for (int i = 0; i < pointsStructDouble.Length / 2; i++)
+            {
+                DistanceStructDouble(pointsStructDouble[i * 2], pointsStructDouble[i * 2 + 1]);
+            }
+            stopWatch.Stop();
+            Console.WriteLine("Обычный метод расчёта дистанции со значимым типом (PointStruct — координаты типа double). Затраченное время: {0}", stopWatch.Elapsed);
+
+            stopWatch.Restart();
+            for (int i = 0; i < pointsStructFloat.Length / 2; i++)
+            {
+                DistanceStructFloat2(pointsStructFloat[i * 2], pointsStructFloat[i * 2 + 1]);
+            }
+            stopWatch.Stop();
+            Console.WriteLine("Метод расчёта дистанции без квадратного корня со значимым типом (PointStruct — координаты типа float). Затраченное время: {0}", stopWatch.Elapsed);
+
+            Console.ReadKey();
+        }
+
+        public class PointClassFloat
+        {
+            public float X { get; set; }
+            public float Y { get; set; }
+        }
+        public struct PointStructFloat
+        {
+            public float X { get; set; }
+            public float Y { get; set; }
+        }
+        public struct PointStructDouble
+        {
+            public double X { get; set; }
+            public double Y { get; set; }
+        }
+
+        //Обычный метод расчёта дистанции со ссылочным типом (PointClass — координаты типа float)
+        public static float DistanceClassFloat(PointClassFloat pointOne, PointClassFloat pointTwo)
+        {
+            float x = pointOne.X - pointTwo.X;
+            float y = pointOne.Y - pointTwo.Y;
+            return MathF.Sqrt((x * x) + (y * y));
+        }
+
+        //Обычный метод расчёта дистанции со значимым типом (PointStruct — координаты типа float)
+        public static float DistanceStructFloat(PointStructFloat pointOne, PointStructFloat pointTwo)
+        {
+            float x = pointOne.X - pointTwo.X;
+            float y = pointOne.Y - pointTwo.Y;
+            return MathF.Sqrt((x * x) + (y * y));
+        }
+
+        //Обычный метод расчёта дистанции со значимым типом (PointStruct — координаты типа double)
+        public static double DistanceStructDouble(PointStructDouble pointOne, PointStructDouble pointTwo)
+        {
+            double x = pointOne.X - pointTwo.X;
+            double y = pointOne.Y - pointTwo.Y;
+            return MathF.Sqrt((float)((x * x) + (y * y)));
+        }
+
+        //Метод расчёта дистанции без квадратного корня со значимым типом(PointStruct — координаты типа float)
+        public static float DistanceStructFloat2(PointStructFloat pointOne, PointStructFloat pointTwo)
+        {
+            float x = pointOne.X - pointTwo.X;
+            float y = pointOne.Y - pointTwo.Y;
+            return (x * x) + (y * y);
+        }
+    }
+}
