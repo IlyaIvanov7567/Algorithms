6a5ea4b74153135886527d22a1c13ff9b3901e56
IlyaIvanov7567 <ilyaivanov7567@gmail.com>
2021-10-13 08:06:27 +0300

    Lesson4_Task2. BinaryTree. GetRoot - done. AddItem - done. GetNodeByValue - done. PrintTree - done. RemoveItem - in progress.

---
+++ Lesson4_Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lesson4_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree tree = new Tree();
+            tree.AddItem(8);
+            tree.AddItem(3);
+            tree.AddItem(10);
+            tree.AddItem(1);
+            tree.AddItem(6);
+            tree.AddItem(14);
+            tree.AddItem(4);
+            tree.AddItem(7);
+            tree.AddItem(13);
+
+            tree.PrintTree(tree.rootNode, 20, 0);
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson4_Task2/Tree.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4_Task2
+{
+    class Tree
+    {
+        public TreeNode rootNode { get; set; }
+        public void AddItem(int value) // добавить узел
+        {
+            TreeNode newNode = new TreeNode { Value = value };
+            TreeNode curNode = rootNode;
+            bool added = false;
+
+            if (curNode == null)
+            {
+                rootNode = newNode;
+                return;
+            }
+
+            do
+            {
+                if (value >= curNode.Value)
+                {
+                    if (curNode.RightChild == null)
+                    {
+                        curNode.RightChild = newNode;
+                        added = true;
+                    }
+                    else
+                    {
+                        curNode = curNode.RightChild;
+                    }
+                }
+                if (value < curNode.Value)
+                {
+                    if (curNode.LeftChild == null)
+                    {
+                        curNode.LeftChild = newNode;
+                        added = true;
+                    }
+                    else
+                    {
+                        curNode = curNode.LeftChild;
+                    }
+                }
+            }
+            while (!added);
+        }
+        public TreeNode GetNodeByValue(int value) //получить узел дерева по значению
+        {
+
+            TreeNode curNode = rootNode;
+            bool finded = false;
+
+            do
+            {
+                if (curNode == null)
+                {
+                    throw new Exception($"Узел со значением {value} не найден");
+                }
+
+                else if (curNode.Value == value)
+                {
+                    finded = true;
+                    return curNode;
+                }
+                else if (curNode.Value > value)
+                {
+                    curNode = curNode.LeftChild;
+                }
+                else if (curNode.Value < value)
+                {
+                    curNode = curNode.RightChild;
+                }
+
+            }
+            while (!finded);
+            return curNode;
+        }
+        public TreeNode GetRoot()
+        {
+            if (rootNode == null)
+                throw new Exception("Дерево не посажено");
+            else
+                return rootNode;
+        }
+        public void PrintTree(TreeNode node, int x, int y, int offset = 0) //вывести дерево в консоль
+        {
+            if (node != null)
+            {
+                if (offset == 0)
+                    offset = x / 2;
+                Console.SetCursorPosition(x, y);
+                Console.WriteLine($"({node.Value})");
+                PrintTree(node.LeftChild, x - offset, y + 1, offset / 2);
+                PrintTree(node.RightChild, x + offset, y + 1, offset / 2);
+            }
+        }
+
+        //public void RemoveItem(int value) // удалить узел по значению
+        //{
+        //    if (rootNode == null)
+        //        throw new Exception("Дерево не посажено");
+        //    TreeNode delNode = GetNodeByValue(value);
+        //    if (delNode.LeftChild == null && delNode.RightChild == null)
+        //    {
+
+        //    }
+        //}
+    }
+}
